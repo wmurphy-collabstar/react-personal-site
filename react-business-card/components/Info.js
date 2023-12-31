475bcaefb9f50d6db36fd11305f67a20dccbed82
wmurphy-collabstar <56129494+wmurphy-collabstar@users.noreply.github.com>
2023-12-31 10:34:49 -0500

    Update Info.js
    Added links to Info section

--- react-business-card/components/Info.js
+++ react-business-card/components/Info.js
@@ -6,13 +6,13 @@ export default function Info () {
             <img alt="Winona's profile pic" src="./my_face.jpeg"/>
             <h1 className="name">Winona Murphy</h1>
             <h4 className="job-title">Aspiring Frontend Developer</h4>
-            <p>No website yet</p>
+            <a href="https://wmurphy-collabstar.github.io/personal-portfolio/">wmurphy-collabstar.github.io/personal-portfolio/</a>
             <div className="buttons">
-                <button className="mail-btn">
+                <button className="mail-btn" onClick="location.href='mailto:wviolinm@gmail.com'">
                     <img src="../icons/Mail.png" alt="Email icon"/>
                     Email
                 </button>
-                <button className="linkedin-btn">
+                <button className="linkedin-btn" onClick="location.href='www.linkedin.com/in/winona-murphy-840048170/'">
                     <img src="../icons/linkedin.png" alt="LinkedIn icon"/>
                     LinkedIn
                 </button>
