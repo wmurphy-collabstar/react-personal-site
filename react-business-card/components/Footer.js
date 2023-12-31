bd4da0db07f45f97c9d583b40ad645b74b12f807
wmurphy-collabstar <56129494+wmurphy-collabstar@users.noreply.github.com>
2023-12-31 10:41:46 -0500

    Update Footer.js
    Added links for my social images

--- react-business-card/components/Footer.js
+++ react-business-card/components/Footer.js
@@ -3,10 +3,10 @@ import React from "react"
 export default function Footer() {
     return (
         <footer>
-            <img src = "../icons/twitter.png" alt="Twitter icon"/>
-            <img src = "../icons/facebook.png" alt="Facebook icon"/>
-            <img src = "../icons/instagram.png" alt="Instagram icon"/>
-            <img src = "../icons/github.png" alt="Github icon"/>
+            <a href="twitter.com/wviolinm"><img src = "../icons/twitter.png" alt="Twitter icon"/></a>
+            <a href="www.facebook.com/profile.php?id=100009450551735"><img src = "../icons/facebook.png" alt="Facebook icon"/></a>
+            <a href="www.instagram.com/wviolinm/"><img src = "../icons/instagram.png" alt="Instagram icon"/></a>
+            <a href="www.github.com/wmurphy-collabstar"><img src = "../icons/github.png" alt="Github icon"/></a>
         </footer>
     )
 }
